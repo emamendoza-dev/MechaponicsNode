d82d471bb9c711877fe570ad3e13eb91cf3abe12
Ferema10 <mendoza1im15@gmail.com>
2023-01-29 13:06:50 -0600

    verification of libraries from Arduino path

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#include <WiFi.h>
+
+void setup()
+{
+   
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+    
 }
